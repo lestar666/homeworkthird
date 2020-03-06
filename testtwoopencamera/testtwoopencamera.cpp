e014f583f31d2731ab6f62d5e078ef274ed6e08d
2077946749 <2077946749@qq.com>
2020-03-06 14:37:46 +0800

    two

---
+++ testtwoopencamera/testtwoopencamera.cpp
@@ -0,0 +1,48 @@
+﻿// testtwoopencamera.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	VideoCapture cap;
+	cap.open(0);
+	
+	if (!cap.isOpened())
+	{
+		std::cout << "fail to open" << std::endl;
+		return -1;
+	}
+	double fps = cap.get(CAP_PROP_FPS);
+	std::cout << "fps" << fps << std::endl;
+	while (1) {
+		cv::Mat frame;
+		bool rSucess = cap.read(frame);
+		if (!rSucess)
+		{
+			std::cout << "fail to read" << std::endl;
+			break;
+		}
+		else
+		{
+			cv::imshow("frame", frame);
+		}
+		waitKey(30);
+	}
+
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
